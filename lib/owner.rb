488c2af1c4b8cd3e8e5686a400cbb318bcb864dc
davidmolina2810 <davidmolina2810@gmail.com>
2020-10-08 10:21:01 -0500

    Completed lab.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
   # code goes here
+
+  attr_reader :name, :species
+
+  @@all = []
+  
+
+  def initialize(name)
+    @name = name
+    @species = 'human'
+    @@all << self
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def self.all
+    return @@all
+  end
+
+  def self.count
+    return @@all.length
+  end
+  
+  def self.reset_all
+    return @@all = []
+  end
+  
 end
